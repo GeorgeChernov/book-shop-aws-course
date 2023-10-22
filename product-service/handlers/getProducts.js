bd04e6c294988b49c3eb927832a9ea8195bc61e1
George Chernov <chernovgosha@gmail.com>
2023-10-22 14:49:06 +0300

    Refactoring: replace scan callbacks with promises

--- product-service/handlers/getProducts.js
+++ product-service/handlers/getProducts.js
@@ -6,7 +6,7 @@ AWS.config.setPromisesDependency(require("bluebird"));
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.getProducts = (event, context, callback) => {
+module.exports.getProducts = async (event, context, callback) => {
   var productsParams = {
     TableName: "products",
     ProjectionExpression: "id, title, description, price",
@@ -17,61 +17,30 @@ module.exports.getProducts = (event, context, callback) => {
     ProjectionExpression: "productId, itemCount",
   };
 
-  console.log("Scanning Products table.");
+  const products = await dynamoDb
+    .scan(productsParams)
+    .promise()
+    .then((data) => data.Items);
+  const stocks = await dynamoDb
+    .scan(stocksParams)
+    .promise()
+    .then((data) => data.Items);
 
-  let products = [];
+  let result = [];
 
-  const onScanProducts = (err, data) => {
-    console.log("In onScanProducts.");
+  for (let i = 0; i < products.length; i++) {
+    const stock = stocks.find((x) => x.productId === products[i].id);
 
-    if (err) {
-      console.log(
-        "Scan failed to load data. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
+    result.push({ ...products[i], count: stock.itemCount });
+  }
 
-      callback(err);
-    } else {
-      console.log("Scan succeeded.", data.Items);
-
-      products = data.Items;
-
-      dynamoDb.scan(stocksParams, onScanStocks);
-    }
-  };
-
-  const onScanStocks = (err, data) => {
-    console.log("In onScanStocks.");
-
-    if (err) {
-      console.log(
-        "Scan failed to load data. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
-
-      callback(err);
-    } else {
-      console.log("Scan succeeded.", data.Items);
-
-      let result = [];
-
-      for (let i = 0; i < products.length; i++) {
-        const stock = data.Items.find((x) => x.productId === products[i].id);
-
-        result.push({ ...products[i], count: stock.itemCount });
-      }
-
-      return callback(null, {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS, POST, GET"
-        },
-        body: JSON.stringify(result),
-      });
-    }
-  };
-
-  dynamoDb.scan(productsParams, onScanProducts);
+  return callback(null, {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    },
+    body: JSON.stringify(result),
+  });
 };
